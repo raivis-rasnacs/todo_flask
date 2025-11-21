b8df127f7f437905eef5631b6943566467dcdb93
raivis-rsn <raivis.rasnacs@edu.jekabpils.lv>
2025-11-21 09:08:16 +0200

    v2

--- app.py
+++ app.py
@@ -11,8 +11,39 @@ app = Flask(__name__)
 @app.route("/tasks", methods = ["GET", "POST"])
 def index():
     if request.method == "POST":
-        ...
-    return render_template("index.html")
+        try:
+            con = sqlite3.connect("db/tasks.db")
+            cur = con.cursor()
+            print("Connection successful")
+
+            sql = """
+            INSERT INTO tasks (task_name, priority, date)
+            VALUES (?, ?, ?);
+            """
+
+            task_name = request.form.get("task_name");
+            task_priority = request.form.get("task_priority");
+            task_date = request.form.get("task_date");
+
+            cur.execute(sql, (task_name, task_priority, task_date))
+            con.commit()
+            con.close()
+        except Exception as e:
+            print(e)
+        return redirect("/tasks")
+
+    try:
+        con = sqlite3.connect("db/tasks.db")
+        cur = con.cursor()
+        sql = "SELECT task_name, priority, date FROM tasks;"
+        data = cur.execute(sql)
+        data = data.fetchall()
+        con.close()
+        #print(data)
+    except Exception as e:
+        print(e)
+
+    return render_template("index.html", data=data)
 
 
 app.run(debug=True)
